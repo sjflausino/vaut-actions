6c5dcd6ad698139ebb2b385dd474c85b371cc2ac
Sandro Flausino <sjunior@vertigo.com.br>
2023-09-20 14:20:11 -0300

    adding terraform source

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
 provider "aws" {
     region = "us-east-1"
     access_key = var.access_key
